55d4384f2ea1cff9a5f866154fd2bae24011dd75
Jose Manuel Casillas Martin <chemanuel_95@hotmail.com>
2020-03-25 22:57:49 -0600

    App compilada

--- covid/src/app.js
+++ covid/src/app.js
@@ -21,17 +21,6 @@ class App extends Component {
     console.disableYellowBox = true;
       Parse.initialize('US6Z8wCHUWHj23MTwRiHzecUhzRUPDCMqVKTWxbA',
      '4kSMGxSabFsPrjKgQgJapYMpy84zKKPjrumTn8Ha');
-     Notifications.registerRemoteNotifications();
-
-     Notifications.events().registerNotificationReceivedForeground((notification: Notification, completion) => {
-       console.log(`Notification received in foreground: ${notification.title} : ${notification.body}`);
-       completion({alert: false, sound: false, badge: false});
-     });
-
-     Notifications.events().registerNotificationOpened((notification: Notification, completion) => {
-       console.log(`Notification opened: ${notification.payload}`);
-       completion();
-     });
 /*
      Notifications.postLocalNotification({
 
